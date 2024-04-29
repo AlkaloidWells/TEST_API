c3a618a0b5902499d836ab9c7ff1982a150c322c
ALkaloidWells <alkaliod111@gmail.com>
2024-04-29 10:32:21 +0100

    server_update

--- utilites/config.py
+++ utilites/config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db', 'database.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'db', 'database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '8IR4M7-R3c74GjTHhKzWODaYVHuPGqn4w92DHLqeYJA'
+
